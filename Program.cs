55203e094cb374c0be5eb653f75b481d6425e082
alex <alexandr.paramon56@gmail.com>
2025-06-18 15:36:41 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Asynchronous_File_Download
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
