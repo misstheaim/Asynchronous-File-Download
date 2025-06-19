e00e6d95090d86321d68f9d607d79aaf46ae8743
alex <alexandr.paramon56@gmail.com>
2025-06-19 16:26:19 +0500

    feat(core): Added main functionality

--- Program.cs
+++ Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Asynchronous_File_Download
+﻿namespace Asynchronous_File_Download;
+
+internal class Program
 {
-    internal class Program
+    static async Task Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Please enter url sources divided by ','.");
+        List<string> urlList = Console.ReadLine().Split(new char[] { ',' }).ToList();
+
+        List<string> trimUrls = new List<string>();
+        foreach (string url in urlList)
         {
-            Console.WriteLine("Hello, World!");
+            trimUrls.Add(url.Trim());
         }
+
+        try
+        {
+            WebService service = new WebService();
+
+            List<Task> tasks = new List<Task>();
+            foreach(string url in trimUrls)
+            {
+                tasks.Add(service.DownloadFileAsync(url));
+            }
+            await Task.WhenAll(tasks);
+            
+        }
+        catch (HttpProtocolException e)
+        {
+            Console.WriteLine("\nDownload is failed, error message:\n{0}", e.Message);
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("\nDownloading canceled.");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("\nUnhandled exception, error message:\n{0}", e.Message);
+        }
+        
     }
 }
---
+++ WebService.cs
@@ -0,0 +1,74 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Asynchronous_File_Download;
+
+internal class WebService
+{
+    private HttpClient _httpClient;
+
+    private CancellationTokenSource _tokenSource;
+
+    private readonly string saveDirectory = "Files";
+
+    public WebService()
+    {
+        _httpClient = new HttpClient();
+        _tokenSource = new CancellationTokenSource();
+        Directory.CreateDirectory(saveDirectory);
+        Task.Run( CancelTask );
+    }
+
+    public async Task DownloadFileAsync(string url)
+    {
+        var response = await _httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+
+        response.EnsureSuccessStatusCode();
+
+        string fileName = GetFileName(url);
+        long totalBytes = response.Content.Headers.ContentLength ?? -1L;
+        long totalBytesRead = 0;
+        int chunkSize = 8192;
+
+        using Stream remoteFileStream = await response.Content.ReadAsStreamAsync();
+        using FileStream localFileStream = new FileStream(fileName, FileMode.OpenOrCreate);
+
+        byte[] buffer = new byte[chunkSize];
+        int bytesRead;
+
+        while((bytesRead = await remoteFileStream.ReadAsync(buffer, 0, chunkSize, _tokenSource.Token)) > 0)
+        {
+            await localFileStream.WriteAsync(buffer, 0, bytesRead, _tokenSource.Token);
+            totalBytesRead += bytesRead;
+            Console.Write("\rBytes read - {0} from - {1}", totalBytesRead, totalBytes);
+        }
+    }
+
+    private string GetFileName(string url)
+    {
+        string fileName = saveDirectory + "/" + Regex.Match(url, "[^/]+(?=/?$)").Value;
+        int i = 0;
+        Match matchGroups = Regex.Match(fileName, @"^([^\.]+)(\..+)$");
+        string originalName = matchGroups.Groups[1].Value;
+        while (File.Exists(fileName))
+        {
+            fileName = originalName;
+            fileName += "(" + i + ")" + matchGroups.Groups[2].Value;
+            i++;
+        }
+        return fileName;
+    }
+
+    private void CancelTask()
+    {
+        Console.WriteLine("Press Esc to cancel downloading.");
+
+        while (Console.ReadKey().Key != ConsoleKey.Escape)
+        {
+            
+        }
+
+        Console.WriteLine("\n EEcs key is pressed: canceling downloading!");
+
+        _tokenSource.Cancel();
+    }
+}
